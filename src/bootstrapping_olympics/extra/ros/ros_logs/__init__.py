decc3f670268e93733c33d84f498df5ae06adcfc
Andrea Censi <andrea@cds.caltech.edu>
2012-03-05 21:39:41 -0800

    move

--- src/bootstrapping_olympics/extra/ros/ros_logs/__init__.py
+++ src/bootstrapping_olympics/extra/ros/ros_logs/__init__.py
@@ -1,9 +1,10 @@
-''' Functions for dealing with ROS logs (currently broken). '''
+''' Functions for dealing with ROS logs. '''
 
 from .. import logger, np, contract
 
 from .index import *
 from .ros_conversions import *
+from .ros2python import *
 from .read import *
 from .write import *
 from .interface import *
