90953e1e7fea4c687abff64e201f287944384cac
Andrea Censi <andrea@cds.caltech.edu>
2012-07-30 12:05:35 -0700

    removed reports

--- src/bootstrapping_olympics/extra/latex/names.py
+++
@@ -1,9 +0,0 @@
-
-
-def bvid(id_component):
-    return "\\bvid{%s}" % id_component
-
-
-def bvrobpretty(id_robot):
-    """ Pretty prints robot ID as formula """
-    return "S(e, %s)" % bvid(id_robot) # FIXME
